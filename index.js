dc92338fbbb85ae5c053c5251c2565e180980b01
Ayush Singh <ayush.singh@smartertravelmedia.com>
2016-11-05 14:22:18 -0400

    NodeJS skeleton

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    bodyParser = require('body-parser'),
+    server = require('http').Server(app);
+
+
+// Configure App
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/img', express.static('public/img'));
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-requested-with,Authorization,Access-Control-Allow-Origin');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(express.static(__dirname + '/public'));
+app.options("*", function(req,res,next) { res.send(200) });
+
+
+// Routes
+app.get('/heartbeat', function(req, res) {
+	res.send("OKOKOK");
+});
+
+app.use('/*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+server.timeout = 0;
+server.listen(8080, function() {
+  console.log('Listening on port %d', server.address().port);
+});
